f2089ff40b1d2b3153a4cb3263b25bd9148f0621
Littlechilde <fhy755247696@qq.com>
2023-02-06 11:03:27 +0800

    initial commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png" />
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+  </view>
+</template>
+
+// 方法一：在 B 页面
+<script setup>
+import { ref } from "vue";
+import { onLoad, onShow } from "@dcloudio/uni-app";
+const title = ref("vue3.0 + uni-app");
+
+// onLoad 接受 A 页面传递的参数
+onLoad((option) => {
+  console.log("B 页面 onLoad:", option); //B 页面 onLoad: {id: '1', name: 'uniapp'}
+});
+
+onShow(() => {
+  console.log("B 页面 onShow");
+});
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
